e61633232c504e3e09a31b934f6b1d8f935604bc
Callum Wilkinson <callum.wilkinson1@gmail.com>
2017-04-03 22:32:38 +0100

    Added handlers for events within the chat app

--- app/scripts/src/app.js
+++ app/scripts/src/app.js
@@ -3,6 +3,13 @@ import socket from './ws-client';
 class ChatApp {
   constructor() {
     socket.init('ws://localhost:3001');
+    socket.registerOpenHandler(() => {
+      let message = new ChatMessage({ message: 'Hello?!'});
+      socket.sendMessage(message.serialize());
+    });
+    socket.registerMessageHandler((data) => {
+      console.log(data);
+    });
   }
 }
 
